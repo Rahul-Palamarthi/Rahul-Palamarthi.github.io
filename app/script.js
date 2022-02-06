273f22e1be06db452a893bc7b91c6c041ab59cf5
Rahul Palamarthi <87606332+Rahul-Palamarthi@users.noreply.github.com>
2022-02-06 23:19:25 +0530

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector("nav");
+const navToggle = document.querySelector(".mobileNavToggle");
+
+navToggle.addEventListener("click", () => {
+	const visibility = nav.getAttribute("data-visible");
+
+	if (visibility === "false") {
+		nav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
+	} else {
+		nav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
+	}
+});
+
