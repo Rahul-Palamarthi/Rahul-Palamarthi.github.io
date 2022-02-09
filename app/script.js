7481cf6bdc2a75629efce695ab1f73d4125c5ce2
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2022-02-10 00:00:46 +0530

    Added more JS and CSS
    1.Added functionality in Navigation Bar using JS.
    2.changed the topography.
    3.changed the back-to-top button.
    4.auto close the navigation bar on selecting links.

--- app/script.js
+++ app/script.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector("nav");
 const navToggle = document.querySelector(".mobileNavToggle");
+const emptyNav = document.querySelector(".emptyNav");
 
 navToggle.addEventListener("click", () => {
 	const visibility = nav.getAttribute("data-visible");
@@ -7,20 +8,51 @@ navToggle.addEventListener("click", () => {
 	if (visibility === "false") {
 		nav.setAttribute("data-visible", true);
 		navToggle.setAttribute("aria-expanded", true);
+		emptyNav.style.display = "inline";
 	} else {
 		nav.setAttribute("data-visible", false);
 		navToggle.setAttribute("aria-expanded", false);
+		emptyNav.style.display = "none";
 	}
 });
 
-const navLinks = document.querySelector(".navUl").children;
+const navLinks = Array.from(document.querySelector(".navUl").children);
 
 for (var i = 0; i < navLinks.length; i++) {
 	navLinks[i].addEventListener("click", () => {
 		nav.setAttribute("data-visible", false);
 		navToggle.setAttribute("aria-expanded", false);
+		emptyNav.style.display = "none";
 	});
 }
+
+emptyNav.addEventListener("click", () => {
+	nav.setAttribute("data-visible", false);
+	navToggle.setAttribute("aria-expanded", false);
+	emptyNav.style.display = "none";
+});
+
+const btnsContainer = document.querySelector(".navUl");
+const btns = btnsContainer.querySelectorAll("li");
+
+btns.forEach((element) => {
+	element.addEventListener("click", () => {
+		const activeElement = document.querySelector(".active");
+		activeElement.classList.remove("active");
+		element.classList.add("active");
+	});
+});
+
+const backToTop = document.querySelector(".backToTop");
+console.log(backToTop);
+
+window.onscroll = () => {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		backToTop.style.display = "inline";
+	} else {
+		backToTop.style.display = "none";
+	}
+};
 
 const inputParent = document.querySelectorAll(".inputContainer");
 const input = Array.from(document.querySelectorAll("input[placeholder]"));
@@ -38,4 +70,3 @@ input.forEach((element) => {
 		inputParent[input.indexOf(element)].style.border = "2px solid var(--lightgrey)";
 	});
 });
-
